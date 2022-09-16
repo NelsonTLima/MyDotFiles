99290126f4f51cfc29346b27a2766b14820ca1ab
NelsonTLima <nelsontlima@gmail.com>
2022-09-16 19:12:31 -0300

    adding python libraries

--- python-libraries.sh
+++ python-libraries.sh
@@ -9,3 +9,19 @@ pip install requests
 echo "installing tqdm"
 
 pip install tqdm
+
+echo "installing matplotlib"
+
+pip install matplotlib
+
+echo "installing numpy"
+
+pip install numpy
+
+echo "installing scipy"
+
+pip install scipy
+
+echo "installing scikit-learn"
+
+pip install scikit-learn
