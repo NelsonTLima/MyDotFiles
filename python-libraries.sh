dbace13b9834508003441f50ead0b2681765952c
NelsonTLima <nelsontlima@gmail.com>
2022-09-21 00:27:10 -0300

    python libraries for pip3 also.

--- python-libraries.sh
+++ python-libraries.sh
@@ -1,27 +1,35 @@
 echo "Upgrading pip"
 
 pip install --upgrade pip
+pip3 install --upgrade pip3
 
 echo "installing requests"
 
 pip install requests
+pip3 install requests
 
 echo "installing tqdm"
 
 pip install tqdm
+pip3 install tqdm
+
 
 echo "installing matplotlib"
 
 pip install matplotlib
+pip3 install matplotlib
 
 echo "installing numpy"
 
 pip install numpy
+pip3 install numpy
 
 echo "installing scipy"
 
 pip install scipy
+pip3 install scipy
 
 echo "installing scikit-learn"
 
 pip install scikit-learn
+pip3 install scikit-learn
